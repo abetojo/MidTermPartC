d71f317da6a26692f07f8bec468693760b64b99a
joshua abeto <abetojo@sheridancollege.ca>
2022-06-21 13:00:22 -0400

    Created Joshua.java Added a hello commit in class block

---
+++ src/midtermexam_versiona_extensioncode/Joshua.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package midtermexam_versiona_extensioncode;
+
+/**
+ * SYST17796 Fundamentals of Software Design and Development
+ * @author Joshua Abeto
+ */
+public class Joshua {
+
+    //Hello Professor
+    //I hope you're having a wonderful day
+}
